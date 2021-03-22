578dc51238ec68f3b7bd8e8459e151f5bbac5730
lou1swu <574637316@qq.com>
2021-03-22 20:47:16 +0800

    feat: add app type mp & h5

---
+++ mp_default/javascript/app.js
@@ -0,0 +1,17 @@
+App({
+  onLaunch(options) {
+    console.log('App Launch', options);
+    console.log('getSystemInfoSync', dd.getSystemInfoSync());
+    console.log('SDKVersion', dd.SDKVersion);
+    this.globalData.corpId = options.query.corpId;
+  },
+  onShow() {
+    console.log('App Show');
+  },
+  onHide() {
+    console.log('App Hide');
+  },
+  globalData: {
+    corpId: '',
+  },
+});
