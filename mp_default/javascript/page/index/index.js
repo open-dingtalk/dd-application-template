15db04cbd7981d67f631a2463d211a1705e16dad
lou1swu <574637316@qq.com>
2021-06-17 14:14:19 +0800

    feat: 小程序和h5增加eslintrc

--- mp_default/javascript/page/index/index.js
+++ mp_default/javascript/page/index/index.js
@@ -1,77 +1,68 @@
 
-let app = getApp();
+const app = getApp();
 
 
-//内网穿透工具介绍:
+// 内网穿透工具介绍:
 // https://open-doc.dingtalk.com/microapp/debug/ucof2g
-//替换成开发者后台设置的安全域名
-let domain = "http://localhost:3000";
-let url = domain + '/login';
+// 替换成开发者后台设置的安全域名
+const domain = 'http://localhost:3000';
+const url = `${domain }/login`;
 
 Page({
-    data:{
-        corpId: '',
-        authCode:'',
-        userId:'',
-        userName:'',
-        hideList: true,
-    },
-    loginSystem() {
-        dd.showLoading();
-        dd.getAuthCode({
-            success:(res)=>{
-                this.setData({
-                    authCode:res.authCode
-                })
-                //dd.alert({content: "step1"});
-                dd.httpRequest({
-                    url: url,
-                    method: 'POST',
-                    data: {
-                        authCode: res.authCode
-                    },
-                    dataType: 'json',
-                    success: (res) => {
-                        // dd.alert({content: "step2"});
-                        console.log('success----',res)
-                        let userId = res.data.result.userId;
-                        let userName = res.data.result.userName;
-                        this.setData({
-                            userId:userId,
-                            userName:userName,
-                            hideList:false
-                        })
-                    },
-                    fail: (res) => {
-                        console.log("httpRequestFail---",res)
-                       dd.alert({content: JSON.stringify(res)});
-                    },
-                    complete: (res) => {
-                        dd.hideLoading();
-                    }
-                    
-                });
-            },
-            fail: (err)=>{
-                // dd.alert({content: "step3"});
-                dd.alert({
-                    content: JSON.stringify(err)
-                })
-            }
-        })
-
-    },
-    onLoad(){
-
-        let _this = this;
-
+  data: {
+    corpId: '',
+    authCode: '',
+    userId: '',
+    userName: '',
+    hideList: true,
+  },
+  loginSystem() {
+    dd.showLoading();
+    dd.getAuthCode({
+      success: (res) => {
         this.setData({
-            corpId: app.globalData.corpId
-        })
-        
-        //dd.alert({content: "step1"});
-         
-        
-        
-    }
-})
+          authCode: res.authCode,
+        });
+        // dd.alert({content: "step1"});
+        dd.httpRequest({
+          url,
+          method: 'POST',
+          data: {
+            authCode: res.authCode,
+          },
+          dataType: 'json',
+          success: (successResult) => {
+            // dd.alert({content: "step2"});
+            const { userId } = successResult.data.result;
+            const { userName } = successResult.data.result;
+            this.setData({
+              userId,
+              userName,
+              hideList: false,
+            });
+          },
+          fail: (failResult) => {
+            dd.alert({ content: JSON.stringify(failResult) });
+          },
+          complete: () => {
+            dd.hideLoading();
+          },
+
+        });
+      },
+      fail: (err) => {
+        // dd.alert({content: "step3"});
+        dd.alert({
+          content: JSON.stringify(err),
+        });
+      },
+    });
+  },
+  onLoad() {
+    this.setData({
+      corpId: app.globalData.corpId,
+    });
+
+    // dd.alert({content: "step1"});
+  },
+});
