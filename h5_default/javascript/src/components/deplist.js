e75e66c3e6195f8198fb424858958962e0b755d8
lou1swu <574637316@qq.com>
2021-06-17 14:14:07 +0800

    feat: 小程序和h5增加eslintrc

--- h5_default/javascript/src/components/deplist.js
+++ h5_default/javascript/src/components/deplist.js
@@ -1,78 +1,82 @@
 import React from 'react';
 import { List, Spin, Avatar, Button } from 'antd';
 import * as dd from 'dingtalk-jsapi';
-import config from '../config.js'
+import config from '../config.js';
 
-const host = config.host
+const { host } = config;
 class H5AppQSDeplist extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            loading: false,
-            users: [
-            ],
-        }
+    this.state = {
+      loading: false,
+      users: [
+      ],
+    };
 
-        this.openChat = this.openChat.bind(this);
-    }
+    this.openChat = this.openChat.bind(this);
+  }
 
-    componentDidMount() {
-        this.setState({loading: true});
-        // 单页应用使用首页地址做jsAPI鉴权
-        dd.config({
-            ...this.props.config,
-            jsApiList: [
-                'biz.chat.openSingleChat'
-            ]
-        })
-        dd.error(err => {
-            alert('dd.config error: ' + JSON.stringify(err));
+  UNSAFE_componentWillMount() {
+    this.setState({ loading: true });
+  }
+
+  componentDidMount() {
+    // 单页应用使用首页地址做jsAPI鉴权
+    dd.config({
+      ...this.props.config,
+      jsApiList: [
+        'biz.chat.openSingleChat',
+      ],
+    });
+    dd.error((err) => {
+      dd.alert(`dd.config error: ${ JSON.stringify(err)}`);
+    });
+
+    fetch(`${host }/user/simplelist?department_id=${ this.props.deptId}`)
+      .then((res) => res.json())
+      .then((result) => {
+        this.setState({
+          users: result.result,
+          loading: false,
         });
+      });
+  }
 
-        fetch(host + '/user/simplelist?department_id='+ this.props.deptId)
-            .then(res => res.json())
-            .then(result => {
-                this.setState({
-                    users: result.result,
-                    loading: false
-                })
-            })
-    }
+  openChat(e, userid) {
+    // alert(JSON.stringify(this.props.config));
+    dd.biz.chat.openSingleChat({
+      corpId: this.props.config.corpId, // 企业id,必须是用户所属的企业的corpid
+      userId: userid, // 用户的工号
+      onSuccess() {},
+      onFail(err) { dd.alert(`fail:${ JSON.stringify(err)}`); },
+    }).catch((err) => dd.alert(`exp:${ JSON.stringify(err) }${JSON.stringify(this.props.config)}`));
+  }
 
-    openChat(e, userid) {
-        //alert(JSON.stringify(this.props.config));
-        dd.biz.chat.openSingleChat({
-            corpId: this.props.config.corpId, // 企业id,必须是用户所属的企业的corpid
-            userId: userid, // 用户的工号
-            onSuccess : function() {},
-            onFail : function(err) {alert("fail:" + JSON.stringify(err));}
-        }).catch(err => alert("exp:" + JSON.stringify(err) + JSON.stringify(this.props.config)))
-    }
-
-    render() {
-        return (
-            <div style={{ width:320, margin: '0 auto' }}>
-                <Spin tips="Loading..." spinning={this.state.loading}>
-                    <h3 style={{ marginBottom: 16 }}>用户列表</h3>
-                    <List
-                        bordered
-                        dataSource={this.state.users}
-                        renderItem={item => (
-                            <List.Item>
-                                <Avatar icon="user" /> {item.name}
-                                <Button
-                                    onClick={(e) => this.openChat(e, item.userid)}
-                                    shape="circle"
-                                    icon="dingding"
-                                    style={{marginLeft: 20}}/>
-                            </List.Item>
-                        )}
-                    />
-                </Spin>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div style={{ width: 320, margin: '0 auto' }}>
+        <Spin tips="Loading..." spinning={this.state.loading}>
+          <h3 style={{ marginBottom: 16 }}>用户列表</h3>
+          <List
+            bordered
+            dataSource={this.state.users}
+            renderItem={(item) => (
+              <List.Item>
+                <Avatar icon="user" /> {item.name}
+                <Button
+                  onClick={(e) => this.openChat(e, item.userid)}
+                  shape="circle"
+                  icon="dingding"
+                  style={{ marginLeft: 20 }}
+                />
+              </List.Item>
+            )}
+          />
+        </Spin>
+      </div>
+    );
+  }
 }
 
-export default H5AppQSDeplist
+export default H5AppQSDeplist;
--- h5_default/javascript/src/components/user.js
+++ h5_default/javascript/src/components/user.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Avatar } from 'antd';
+import { get } from 'lodash';
 
 function H5AppQSUser(props) {
-    return (
-        <div className="user" style={props.style}>
-            <Avatar size="large" icon="user" src={props.user.avatar}/>
-            <span className="user-detail">
-                <span>{props.user.name}</span> (<span>{props.user.userid}</span>)
-            </span>
-        </div>
-    )
+  return (
+    <div className="user" style={get(props, 'style')}>
+      <Avatar size="large" icon="user" src={get(props, 'user.avatar')} />
+      <span className="user-detail">
+        <span>{get(props, 'user.name')}</span> (<span>{get(props, 'user.userid')}</span>)
+      </span>
+    </div>
+  );
 }
 
-export default H5AppQSUser
+export default H5AppQSUser;
