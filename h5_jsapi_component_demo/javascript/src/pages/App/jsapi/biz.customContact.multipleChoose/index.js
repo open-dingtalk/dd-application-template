341e36558ca06c2247900d7564e6c8cf7874c4a3
zequan.wzq <zequan.wzq@alibaba-inc.com>
2022-01-18 15:35:32 +0800

    fix(h5): biz.customContact.multipleChoose示例修改

--- h5_jsapi_component_demo/javascript/src/pages/App/jsapi/biz.customContact.multipleChoose/index.js
+++ h5_jsapi_component_demo/javascript/src/pages/App/jsapi/biz.customContact.multipleChoose/index.js
@@ -1,16 +1,30 @@
 import React, { useState, useCallback } from 'react';
 import * as dd from 'dingtalk-jsapi';
-import { Button, } from 'dingtalk-design-mobile';
+import { Button, Form, InputItem, } from 'dingtalk-design-mobile';
 
 const ApiComponent = () => {
-    const onHandleClick = () =>{
+    const onHandleClick = (values) =>{
       dd.biz.customContact.multipleChoose({
           corpId: window.__corpId__,
+          users: values.userIds.split(','),
+          disabledUsers: [],
       });
     }
 
   return (
-    <Button onClick={onHandleClick}>选择联系人</Button>
+    <Form
+        initialValues={{ userId: '' }}
+        onFinish={(values) => onHandleClick(values)}
+      >
+        <Form.Group renderHeader="表单">
+          <Form.Item label="UserIds" name="userIds" position="brief">
+            <InputItem placeholder="请输入多个联系人的userId，按,分隔" clear maxLength={50}/>
+          </Form.Item>
+        </Form.Group>
+        <Button type="primary" htmlType="submit">
+            多选自定义联系人
+        </Button>
+      </Form>
   );
 };
 
