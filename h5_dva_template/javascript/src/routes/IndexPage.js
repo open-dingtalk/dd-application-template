96390a12939f4d3600ace5dc9a8517b821dfea23
zhouzijie.zzj <zhouzijie.zzj@alibaba-inc.com>
2022-01-21 16:19:32 +0800

    fix:升级dva工程化配置

--- h5_dva_template/javascript/src/routes/IndexPage.js
+++ h5_dva_template/javascript/src/routes/IndexPage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { connect } from 'dva';
-import styles from './IndexPage.css';
+import './IndexPage.css';
 
 function IndexPage() {
   return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Hi! 这是一个简单示例</h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
+    <div className="normal">
+      <h1 className="title">Hi! 这是一个简单示例</h1>
+      <div className="welcome" />
+      <ul className="list">
         <li>可以从<code>src/index.js</code>继续您的开发</li>
         <li>更多参考：</li>
         <li><a href="https://open.dingtalk.com/document/org/application-types">钉钉开放平台应用开发</a></li>
